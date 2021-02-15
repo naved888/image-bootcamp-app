caaa4d4820f6972d13d36b10a496e393a39f92c8
naved888 <navedshaikh@credenceanalytics.com>
2021-02-15 05:53:40 +0530

    Chore:addedfooter

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav-app ></nav-app>
     <upload-image-app ></upload-image-app>
     <display-image-app ></display-image-app>
+    <footer-app></footer-app>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import Nav from './components/Nav'
 import UploadImage from './components/UploadImage'
 import DisplayImage from './components/DisplayImage'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     "nav-app": Nav,
     "upload-image-app": UploadImage,
-    "display-image-app": DisplayImage
+    "display-image-app": DisplayImage,
+    "footer-app": Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>===> &copy;NavedShaikh</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    text-align: center;
+    font-weight: bold;
+}
+</style>
